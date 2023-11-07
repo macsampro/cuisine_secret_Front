30cc5c482a416728c6e3013f6f7572acfce6cd98
macsampro <112323681+macsampro@users.noreply.github.com>
2023-11-07 15:13:11 +0100

    mise en place du service photos

--- src/app/services/favoris.service.ts
+++ src/app/services/favoris.service.ts
@@ -1,9 +1,68 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Favoris } from '../models/favoris';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavorisService {
+  urlApi: string = 'http://localhost:3000/favoris';
+  recipes: Favoris[] = [];
 
-  constructor() { }
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('access_token');
+    let headers = new HttpHeaders();
+    if (token) {
+      headers = headers.set('Authorization', 'Bearer ' + token);
+    }
+    return headers;
+  }
+
+
+
+  constructor(private http: HttpClient) { }
+
+
+
+
+  getAllFavoris(): Observable<Favoris[]> {
+    return this.http.get<Favoris[]>(`${this.urlApi}`, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  getFavorisById(id: number): Observable<Favoris> {
+    return this.http.get<Favoris>(`${this.urlApi}/${id}`, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  addFavoris(favoris: Favoris): Observable<Favoris> {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+    });
+    return this.http.post<Favoris>(`${this.urlApi}`, favoris, {
+      headers: headers,
+    });
+  }
+
+  modifyFavoris(
+    id: number,
+    updateData: Partial<Favoris>
+  ): Observable<Partial<Favoris>> {
+    return this.http.patch<Favoris>(`${this.urlApi}/${id}`, updateData, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  deleteFavoris(id: number) {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+    });
+    return this.http.delete(`${this.urlApi}/${id}`, {
+      headers: headers,
+    });
+  }
+
 }
--- src/app/services/photos.service.ts
+++ src/app/services/photos.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { Photos } from '../models/photos';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotosService {
+  urlApi: string = 'http://localhost:3000/photos';
+  photos: Photos[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getImage() {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+    });
+
+    return this.http.get(`${this.urlApi}`, {
+      responseType: 'blob',
+      headers: headers,
+    });
+  }
+  getImageById(id: number) {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+    });
+
+    return this.http.get(`${this.urlApi}/${id}`, {
+      responseType: 'blob',
+      headers: headers,
+    });
+  }
+
+  postImage(formData: FormData) {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+    });
+    return this.http.post(`${this.urlApi}`, formData, {
+      headers: headers,
+    });
+  }
+
+  deletePhotos(id: number) {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+    });
+    return this.http.delete(`${this.urlApi}/${id}`, {
+      headers: headers,
+    });
+  }
 }
