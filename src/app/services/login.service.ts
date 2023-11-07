3d2f39517fc2f6c233abcb7786396058dd1da305
macsampro <112323681+macsampro@users.noreply.github.com>
2023-11-07 15:32:42 +0100

    mise en place du loginService

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Login } from '../models/login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+ private urlApi = 'http://localhost:3000/auth';
+
+ isConnected: boolean = false;
+
+ constructor(private http: HttpClient) {}
+ login(username: string, password_hash: string): Observable<Login> {
+   const body = { username: username, password: password_hash };
+      return this.http.post<Login>(this.urlApi + '/login', body);
+ }
+
+ checkConnexion(): boolean{
+   this.isConnected = !!localStorage.getItem('access_token');
+   return this.isConnected;
+ }
+
+
+}
