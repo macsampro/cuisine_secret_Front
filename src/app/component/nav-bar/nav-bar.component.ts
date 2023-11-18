20f11ed42a92e99dc5bdc0efffa687d7b5f03441
macsampro <112323681+macsampro@users.noreply.github.com>
2023-11-18 23:53:46 +0100

    mise en plce de la deconexion ok

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+
+  constructor(private router:Router){}
+  logout() {
+    // Supprimer le token du stockage local
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('username');
+    // Rediriger l'utilisateur vers la page de connexion ou la page d'accueil
+    this.router.navigate(['/login_page']);
+  }
+
 
 }
